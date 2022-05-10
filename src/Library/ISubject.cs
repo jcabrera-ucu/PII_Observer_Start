0ee8f29bfa706ea67d618d92dcdec43c54b1c5ed
Juan Cabrera <j.cabrera@correo.ucu.edu.uy>
2022-05-10 18:41:06 -0300

    Impl

---
+++ src/Library/ISubject.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Observer
+{
+    public interface ISubject<T>
+    {
+        void Subscribe(IObserver<T> observer);
+
+        void Unsubscribe(IObserver<T> observer);
+    }
+}
